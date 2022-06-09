cd657fe37aecebe77ed47f3b4e08910828a2a60d
anysg <2801772839@qq.com>
2022-06-09 21:54:05 +0800

    新的初始化

--- config/config.default.js
+++ config/config.default.js
@@ -13,7 +13,7 @@ module.exports = appInfo => {
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1654781889415_115';
+  config.keys = appInfo.name + '_1654782678323_8299';
 
   // add your middleware config here
   config.middleware = [];

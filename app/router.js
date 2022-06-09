d8f0959d608e158c3c9ccaeebb99a0278d837bca
anysg <2801772839@qq.com>
2022-06-09 22:02:29 +0800

    router

--- app/router.js
+++ app/router.js
@@ -1,9 +1,4 @@
-'use strict';
-
-/**
- * @param {Egg.Application} app - egg application
- */
 module.exports = app => {
-  const { router, controller } = app;
-  router.get('/', controller.home.index);
-};
+  const { router, controller } = app
+}
+router.get('/', controller.home.index)
